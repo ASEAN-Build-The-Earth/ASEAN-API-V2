742ad1b5f0ff24344b2cd7a2f6daff9abbe4d9bd
Kira <mukeshreddy.9.new@gmail.com>
2021-12-10 15:38:03 +0530

    [fix] deleted src dir

--- src/scripts/builds/builds.js
+++
@@ -1,9 +0,0 @@
-function randomObject(obj) {
-    const arr = Object.values(obj);
-    const randomValue = Math.floor(Math.random() * arr.length);
-    document.getElementById("content").innerHTML = JSON.stringify(arr[randomValue]);
-}
-
-$.getJSON("../../../data/builds/compressed/builds.min.json", function (data) {
-    randomObject(data);
-})
